7ce70f3fff23d075203d3af8b9f1a712cabed2b9
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-07-29 11:27:48 -0400

    AB#205: Add NuGet to release deployments (#2)
    * AB#205: Add NuGet to release deployments
    
    * AB#205: Get NuGet OIDC token in release workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+* 2026-07-29 - AB#205: Get NuGet OIDC token in release workflow.
+* 2026-07-29 - AB#205: Add NuGet to release deployments.
 * 2026-06-26 - AB#157: CI/CD workflows and unit tests.
 * 2026-06-26 - AB#157: Update project description
 * 2026-06-26 - AB#157: Make README visible in Solution Explorer
---
+++ LogicBuilder.App.Maui.Forms.Parameters/DummyFormParametersConstructor.cs
@@ -0,0 +1,34 @@
+﻿using LogicBuilder.App.Maui.Forms.Parameters.Bindings;
+using LogicBuilder.App.Maui.Forms.Parameters.DataForm;
+using LogicBuilder.App.Maui.Forms.Parameters.SearchForm;
+using LogicBuilder.App.Maui.Forms.Parameters.TextForm;
+
+namespace LogicBuilder.App.Maui.Forms.Parameters
+{
+#pragma warning disable S107//used for configuration by reflection
+#pragma warning disable CS9113//used for configuration by reflection
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class DummyFormParametersConstructor(
+        CommandButtonParameters commandButtonParameters,
+        DropdownSelectorControlSettingsParameters dropdownSelectorControlSettingsParameters,
+        FormGroupArraySettingsParameters formGroupArraySettingsParameters,
+        FormGroupSettingsParameters formGroupSettingsParameters,
+        FormGroupBoxSettingsParameters groupBoxSettingsParameters,
+        InputFieldControlSettingsParameters inputFieldControlSettingsParameters,
+        MultiSelectFormControlSettingsParameters multiSelectFormControlSettingsParameters,
+        SearchFilterGroupParameters searchFilterGroupParameters,
+        SearchFilterParameters searchFilterParameters,
+        FormattedLabelItemParameters formattedLabelItemParameters,
+        HyperLinkLabelItemParameters hyperLinkLabelItemParameters,
+        LabelItemParameters labelItemParameters,
+        HyperLinkSpanItemParameters hyperLinkSpanItemParameters,
+        SpanItemParameters spanItemParameters,
+        MultiSelectItemBindingParameters multiSelectItemBindingParameters,
+        DropDownItemBindingParameters dropDownItemBindingParameters,
+        TextItemBindingParameters textItemBindingParameters
+        )
+#pragma warning restore S107
+#pragma warning restore CS9113
+    {
+    }
+}
